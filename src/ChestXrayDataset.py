dc6f43a66dab835c4f1800b78777ab2fa400e6d3
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-03-10 13:36:12 +0000

    Bulk commit
    - Added 256x256 18000 images in png format
    - Data pre-processing
    - ChestXrayDataset creation
    - Resnet18 Model importation and setting up
    - Model training and testing

---
+++ src/ChestXrayDataset.py
@@ -0,0 +1,37 @@
+# IMPORTS=
+import cv2
+import pandas as pd
+from torch.utils.data import Dataset
+
+class ChestXrayDataset(Dataset):
+    def __init__(self, csv_file, data_dir, transform=None):
+        # Set the main directory, the csv file and the transformation pipeline:
+        self.transform = transform
+        self.data_dir = data_dir
+        self.csv_file = csv_file
+
+        # Read the csv file:
+        self.df = pd.read_csv(self.data_dir + self.csv_file)
+
+        # Set the labels and the images:
+        self.images = []
+        self.labels = []
+        for i, row in self.df.iterrows():
+            image_path = self.data_dir + "train/" + row["image_id"] + ".png"
+            img = cv2.imread(image_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            if self.transform:
+                img = self.transform(img)
+            self.images.append(img)
+            self.labels.append(row["class_id"])
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        return self.images[idx], self.labels[idx]
+
+def main():
+    pass
+if __name__ == "__main__":
+    main()
